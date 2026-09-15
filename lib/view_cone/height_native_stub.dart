64bdf8944dfcef34c2039b94f4335e527e67dbc3
Dara Adedeji <daraadedeji07@gmail.com>
2026-09-15 11:05:21 -0400

    Allow Flutter web builds to use Dart sightline calculations

---
+++ lib/view_cone/height_native_stub.dart
@@ -0,0 +1,24 @@
+import 'dart:typed_data';
+
+class HeightFrame {
+  HeightFrame(this.stamp, this.positions, this.offsets, this.timings);
+  final int stamp;
+  final Float32List positions;
+  final Uint32List offsets;
+  final Map<String, num> timings;
+  Float32List cone(int index) =>
+      Float32List.sublistView(positions, offsets[index], offsets[index + 1]);
+}
+
+String heightNativeLibraryPath() =>
+    throw UnsupportedError('Native height workers require desktop.');
+
+class HeightNativeWorker {
+  final Map<String, Object?> info = const {};
+  static Future<HeightNativeWorker> open(
+          String dll, String folder, int workers) async =>
+      throw UnsupportedError('Native height workers require desktop.');
+  Future<HeightFrame> compute(int stamp, Float64List queries) async =>
+      throw UnsupportedError('Native height workers require desktop.');
+  Future<void> close() async {}
+}
